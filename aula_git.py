2ad20f6d70833db7b19eba25f7d35c9b9b30cfe0
joseaugusto92 <gutto.olveira@gmail.com>
2025-04-01 17:28:12 -0300

    initial commit

---
+++ aula_git.py
@@ -0,0 +1,9 @@
+print("Calculadora simples")
+
+num1 = float(input("Insira o primeiro numero: "))
+num2 = float(input("Insira o segundo numero: "))
+
+soma(num1, num2)
+subtrai(num1, num2)
+multiplica(num1, num2)
+divide(num1, num2)
